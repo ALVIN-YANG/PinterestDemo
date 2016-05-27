3131295a3dd4ca8c62e62e93bcc13c36f98ddef1
杨卢青 <YLQ@ALVIN-YANG.local>
2016-05-28 00:05:24 +0800

    Begin

---
+++ Pinterest-Demo/Pinterest-Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pinterest-Demo
+//
+//  Created by 杨卢青 on 16/5/28.
+//  Copyright © 2016年 杨卢青. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Pinterest-Demo/Pinterest-Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Pinterest-Demo
+//
+//  Created by 杨卢青 on 16/5/28.
+//  Copyright © 2016年 杨卢青. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
