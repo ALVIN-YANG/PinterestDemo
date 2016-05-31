3b3bc48e8855a29a74f06b161597345820304d1d
杨卢青 <YLQ@ALVIN-YANG.local>
2016-05-31 10:21:45 +0800

    第二个视图的滑动切换, 完美融合神奇转场功能

---
+++ Pinterest-Demo/Pinterest-Demo/Classes/PinBoard(主页)/Controller/PinDetailViewCell.h
@@ -0,0 +1,13 @@
+//
+//  PinDetailViewCell.h
+//  Pinterest-Demo
+//
+//  Created by 杨卢青 on 16/5/30.
+//  Copyright © 2016年 杨卢青. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PinDetailViewCell : UICollectionViewCell
+
+@end
---
+++ Pinterest-Demo/Pinterest-Demo/Classes/PinBoard(主页)/Controller/PinDetailViewCell.m
@@ -0,0 +1,18 @@
+//
+//  PinDetailViewCell.m
+//  Pinterest-Demo
+//
+//  Created by 杨卢青 on 16/5/30.
+//  Copyright © 2016年 杨卢青. All rights reserved.
+//
+
+#import "PinDetailViewCell.h"
+
+@implementation PinDetailViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
